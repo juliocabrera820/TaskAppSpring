59d39733e579dbca1a5f74e1c9fe8c9bd3da1915
juliocabrera820 <juliocabrera820@gmail.com>
2019-08-06 12:06:36 -0500

    Conexion a la base de datos

---
+++ src/java/config/Conexion.java
@@ -0,0 +1,22 @@
+
+package config;
+
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+/**
+ *
+ * @author julio-cabrera
+ */
+public class Conexion {
+    
+    public DriverManagerDataSource conectar(){
+        
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/People");
+        dataSource.setUsername("root");
+        dataSource.setPassword("12345");
+        return dataSource;
+    }
+    
+}
